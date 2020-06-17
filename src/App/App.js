1a79fab05988e80a94ee81b39b44a30f26653113
delindell <daviselindell@gmail.com>
2020-06-16 19:36:28 -0500

    can login, updated uids on seed data

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,61 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
 
 import Auth from '../components/Auth/Auth';
+import fbConnection from '../helpers/data/connection';
 
 import './App.scss';
 
+
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = (props) => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />));
+  return <Route {...rest} render={(props) => routeChecker(props)} />;
+};
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+
     return (
       <div className="App">
         <h1>House Plant Shizz</h1>
-        <Auth />
+        <Auth component={Auth} authed={authed}/>
       </div>
     );
   }
--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -5,11 +5,17 @@ import 'firebase/auth';
 import './Auth.scss';
 
 class Auth extends React.Component {
+  loginClickEvent = (e) => {
+    e.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
   render() {
     return (
       <div className="Auth text-center">
         <h1>Auth Page</h1>
-        <button className="btn btn-danger">Google Login</button>
+        <button className="btn btn-danger" onClick={this.loginClickEvent}>Google Login</button>
       </div>
     );
   }
